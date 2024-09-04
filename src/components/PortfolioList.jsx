0e8fe62e9285bb32619895beaf1be119daa62098
Dami K <dmk9104@gmail.com>
2024-09-04 23:44:36 +0900

    app finishing.

--- src/components/PortfolioList.jsx
+++ src/components/PortfolioList.jsx
@@ -15,7 +15,13 @@ const PortfolioList = () => {
                 </h2>
                 <Projects setActiveMenu={setActiveMenu}/>
                 <Scene activeMenu={activeMenu}/>
-                <div className="lg:h-[30vh] sm:h-[10vh]"></div>
+                
+                <button className="!lg:mb-0 !sm:mb-20 text-white block mx-auto rounded-xl border px-5 py-5 z-!10 hover:bg-white hover:text-black">More Projects{'  '}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor" className="size-3 inline-flex">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                    </svg>
+                </button>
+                <div className="lg:h-[30vh] !sm:h-[10vh]"></div>
             </main>
             {/* Portfolio Projects */}
         </>
